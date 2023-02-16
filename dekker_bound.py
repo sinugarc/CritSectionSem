18252467752bae8aab76e6246f4d5fba4726e929
Danima20 <danima20@ucm.es>
2023-02-16 09:43:55 +0100

    Bounded

--- dekker_bound.py
+++ dekker_bound.py
@@ -1,45 +1,34 @@
 from multiprocessing import Process
 from multiprocessing import current_process
 from multiprocessing import Value, Array
+from multiprocessing import BoundedSemaphore
 
 N = 8
 
-def is_anybody_inside(critical, tid):
-    found = False
-    i = 0
-    while i<len(critical) and not found:
-        found = tid!=i and critical[i]==1
-        i += 1
-    return found
 
-def task(common, tid, critical, turn):
+
+def task(common, tid, turn,bounded):
     a = 0
-    for i in range(10):
+    for i in range(60):
         print(f'{tid}−{i}: Non−critical Section')
         a += 1
         print(f'{tid}−{i}: End of non−critical Section')
-        critical[tid] = 1
-        while is_anybody_inside(critical, tid):
-            critical[tid] = 0
-            print(f'{tid}−{i}: Giving up')
-            while turn.value==tid:
-                pass
-            critical[tid] = 1
+        bounded.acquire()
         print(f'{tid}−{i}: Critical section')
         v = common.value + 1
         print(f'{tid}−{i}: Inside critical section')
         common.value = v
         print(f'{tid}−{i}: End of critical section')
-        critical[tid] = 0
-        turn.value = tid
+        bounded.release()
         
 def main():
     lp = []
+    bounded=BoundedSemaphore(2)
     common = Value('i', 0)
     critical = Array('i', [0]*N)
     turn = Value('i', 0)
     for tid in range(N):
-        lp.append(Process(target=task, args=(common, tid, critical, turn)))
+        lp.append(Process(target=task, args=(common, tid, turn,bounded)))
     print (f"Valor inicial del contador {common.value}")
     for p in lp:
         p.start()
